fe306c297a0dd174bb3d454520d939ac8a4ee4d1
viewtorino <itoh4126@yoy26.local>
2017-06-03 19:43:09 +0900

    Add Base Theme

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("sass", function() {
+    gulp.src("sass/**/*.scss")
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest("./css"));
+});
+
+gulp.task("default", function() {
+    gulp.watch("sass/**/*.scss",["sass"]);
+});
